f3083da68f0f47358aa1818c7107af513ff22b18
rauts <tobias.rau@visus.uni-stuttgart.de>
2024-03-26 13:04:09 +0100

    fixed input error

--- CalculateTrajectory.py
+++ CalculateTrajectory.py
@@ -3,9 +3,15 @@ from tblite.ase import TBLite
 from ase.optimize import BFGS
 
 
-
-def run_calculation(str: input, method = "GFN1-xTB", output = "trajectory.traj"):
-    atoms = read( input )
-    atoms.calc = TBLite( method = method )
-    dynamics = BFGS( atoms, trajectory = output )
-    dynamics.run( fmax = 0.05 )
+class TrajectoryCalculator():
+    
+    def __init__(self, file, method = "GFN1-xTB", output = "trajectory.traj"):
+        self.file = file
+        self.method = method
+        self.output = output
+    
+    def run_calculation(self):
+        atoms = read( self.file )
+        atoms.calc = TBLite( method = self.method )
+        dynamics = BFGS( atoms, trajectory = self.output )
+        dynamics.run( fmax = 0.05 )
